827854f92a9d4ce624568fbd82c9b60da6762920
José Antônio <antoniojaraujo0@gmail.com>
2025-04-01 20:51:00 -0300

    first commit

---
+++ codigo10.py
@@ -0,0 +1,13 @@
+# for num in range(1, 11):
+#     if num % 2 == 0:
+#         pass
+#     else:
+#         print(num)
+# print('Laço encerrado')
+
+s = 0
+a = 1
+while s < 5:
+    s = 3*a
+    a += 1
+    print(s)
